0873e600407505f2d0141d17f13682763d41156a
Daniel Dodmasej <dodmasejdaniel@gmail.com>
2023-10-23 17:15:11 +0200

    new example file for demonstration

--- lib/models/item_model.dart
+++ lib/models/item_model.dart
@@ -2,6 +2,8 @@ import 'package:hive/hive.dart';
 
 part 'item_model.g.dart';
 
+
+
 @HiveType(typeId: 0)
 class Item {
   @HiveField(0)
@@ -13,5 +15,11 @@ class Item {
   @HiveField(2)
   final String category;
 
-  Item({required this.name, required this.location, required this.category});
+  @HiveField(3)
+  final double latitude;
+
+  @HiveField(4)
+  final double longitude;
+
+  Item({required this.name, required this.location, required this.category, required this.latitude, required this.longitude});
 }
--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,31 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import '../global_data.dart'; // Make sure you import this
+import 'package:hive/hive.dart';
+import '../models/item_model.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
 
 class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Sample latitude and longitude for demonstration
+    double latitude = 51.5074;  // London latitude
+    double longitude = -0.1278;  // London longitude
+
     return ValueListenableBuilder(
       valueListenable: Hive.box<Item>('items').listenable(),
       builder: (context, Box<Item> box, _) {
         // Convert items to markers
         List<Marker> markers = box.values.map((item) {
-          return Marker(
-            width: 80.0,
-            height: 80.0,
-            point: LatLng(/* latitude */, /* longitude */),
-            builder: (ctx) => Container(
-              child: Icon(Icons.pin_drop),
-            ),
-          );
-        }).toList();
+  return Marker(
+    width: 200.0,
+    height: 200.0,
+    point: LatLng(item.latitude, item.longitude),  // Use latitude and longitude from the item
+    builder: (ctx) => Container(
+      child: Icon(
+        Icons.pin_drop,
+        size: 100.0,
+        color: Colors.red,
+      ),
+    ),
+  );
+}).toList();
 
         return Scaffold(
           appBar: AppBar(title: Text('Map')),
           body: FlutterMap(
             options: MapOptions(
-              center: LatLng(51.5, -0.09),
+              center: LatLng(latitude, longitude),  // Center the map here
               zoom: 13.0,
             ),
             layers: [
